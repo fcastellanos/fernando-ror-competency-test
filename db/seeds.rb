3dfc025decadad5e3209a7077d70e6d96d5e08b2
Fernando Castellanos <fernando.castellanos@gmail.com>
2020-11-23 17:25:29 -0800

    Adding user roles through Petergate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create(
+    first_name: 'John',
+    last_name: 'Admin',
+    email: 'john@admins.com',
+    password: 'letme1n',
+    password_confirmation: 'letme1n',
+    roles: 'admin'
+)
+
+articles = Article.create([
+    { 
+        category: 'ruby', 
+        title: 'Intro', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'ruby', 
+        title: 'Data Types', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'ruby', 
+        title: 'Flow Control', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'ruby', 
+        title: 'Intermediate', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'rails', 
+        title: 'Intro', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'rails', 
+        title: 'Active Record', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'rails', 
+        title: 'Routing', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+    { 
+        category: 'rails', 
+        title: 'Intermediate', 
+        content: Faker::Lorem.paragraph_by_chars(number: 512, supplemental: false), 
+        user: admin 
+    },
+])
