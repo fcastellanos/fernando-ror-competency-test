3233f235adbdd0ed246260bb5d9b2e3cc686d30c
Fernando Castellanos <fernando.castellanos@gmail.com>
2020-11-29 20:16:03 -0800

    Styling with TailwindCSS - Migrating erb templates to slim

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    future: {
+      // removeDeprecatedGapUtilities: true,
+      // purgeLayersByDefault: true,
+    },
+    purge: [],
+    target: 'relaxed',
+    prefix: '',
+    important: false,
+    separator: ':',
+    theme: {
+    },
+    variants: {
+    },
+    corePlugins: {},
+    plugins: [],
+  }
+  
